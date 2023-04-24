6c77c83ef8f391f52a17416370347bb7e9b55d8d
Bakr Ibrahimi <bakribra5@gmail.com>
2023-04-24 21:18:01 +0100

    task 0: %c, %%, %s

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	_printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'a');
+	printf("Character:[%c]\n", 'a');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
+	printf("hello [%c] oh\n", '\n');
+	_printf("hello [%c] oh\n", '\n');
+	return (0);
+}
